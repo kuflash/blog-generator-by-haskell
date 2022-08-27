8021d11cdc7816d5ced516f37213a3e6b6a2d812
Igor Uvarov <kuflash@ya.ru>
2022-08-27 16:34:19 +0300

    chapter 3.4

--- hello.hs
+++ hello.hs
@@ -1,30 +1,46 @@
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure first) (Structure second) =
+  Structure (first <> second)
+
+render :: Html -> String
+render (Html html) = html
+
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html_ :: String -> String
-html_ = el "html"
+html_ :: Title -> Structure -> Html
+html_ title (Structure content) =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" content
+        )
+    )
 
-head_ :: String -> String
-head_ = el "head"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
-title_ :: String -> String
-title_ = el "title"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-body_ :: String -> String
-body_ = el "body"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-p_ :: String -> String
-p_ = el "p"
-
-makeHtml :: String -> String -> String
-makeHtml title content =
-  html_ (head_ (title_ title) <> body_ content)
-
-myhtml :: String
-myhtml = makeHtml "My page title" (h1_ "I'm a header" <> p_ "I'm a paragraph")
+myhtml :: Html
+myhtml =
+  html_
+    "Page title"
+    ( append_
+        (h1_ "I'm a heading")
+        ( append_
+            (p_ "I'm a paragraph one")
+            (p_ "I'm a paragraph two")
+        )
+    )
 
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)

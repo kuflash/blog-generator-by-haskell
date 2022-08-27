3653eaff450845ff288aecefe3ee1c92cbb649d5
Igor Uvarov <kuflash@ya.ru>
2022-08-27 15:48:03 +0300

    chapter 3.2

--- hello.hs
+++ hello.hs
@@ -1,15 +1,30 @@
-html_ content = "<html>" <> content <> "</html>"
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-head_ content = "<head>" <> content <> "</head>"
+html_ :: String -> String
+html_ = el "html"
 
-title_ content = "<title>" <> content <> "</title>"
+head_ :: String -> String
+head_ = el "head"
 
-body_ content = "<body>" <> content <> "</body>"
+title_ :: String -> String
+title_ = el "title"
 
+body_ :: String -> String
+body_ = el "body"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+p_ :: String -> String
+p_ = el "p"
+
+makeHtml :: String -> String -> String
 makeHtml title content =
   html_ (head_ (title_ title) <> body_ content)
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml :: String
+myhtml = makeHtml "My page title" (h1_ "I'm a header" <> p_ "I'm a paragraph")
 
 main :: IO ()
 main = putStrLn myhtml

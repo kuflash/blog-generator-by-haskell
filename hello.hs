0dfb9f0f1841fc4f179c52c4ea617d997b2ea6d1
Igor Uvarov <kuflash@ya.ru>
2022-08-27 15:33:44 +0300

    chapter 3.1

--- hello.hs
+++ hello.hs
@@ -1,4 +1,15 @@
-myhtml = "<html><body>Hello, world!</body></html>"
+html_ content = "<html>" <> content <> "</html>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+makeHtml title content =
+  html_ (head_ (title_ title) <> body_ content)
+
+myhtml = makeHtml "My page title" "My page content"
 
 main :: IO ()
 main = putStrLn myhtml

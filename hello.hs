9bf6acbd3c5501c116c08666267d4a319312603c
Igor Uvarov <kuflash@ya.ru>
2022-08-28 11:58:42 +0300

    chapter 3.5

--- hello.hs
+++ hello.hs
@@ -1,34 +1,7 @@
-newtype Html = Html String
+import Html
 
-newtype Structure = Structure String
-
-type Title = String
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure first) (Structure second) =
-  Structure (first <> second)
-
-render :: Html -> String
-render (Html html) = html
-
-el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-html_ :: Title -> Structure -> Html
-html_ title (Structure content) =
-  Html
-    ( el
-        "html"
-        ( el "head" (el "title" title)
-            <> el "body" content
-        )
-    )
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
+main :: IO ()
+main = putStrLn (render myhtml)
 
 myhtml :: Html
 myhtml =
@@ -41,6 +14,3 @@ myhtml =
             (p_ "I'm a paragraph two")
         )
     )
-
-main :: IO ()
-main = putStrLn (render myhtml)

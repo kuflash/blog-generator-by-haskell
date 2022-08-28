0de537f66efaaeecbadaf7e2c3ff6f0f41295b80
Igor Uvarov <kuflash@ya.ru>
2022-08-28 12:10:32 +0300

    chapter 3.6

--- Html.hs
+++ Html.hs
@@ -29,18 +29,30 @@ render (Html html) = html
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concat . map escapeChar
+
 html_ :: Title -> Structure -> Html
 html_ title content =
   Html
     ( el
         "html"
-        ( el "head" (el "title" title)
+        ( el "head" (el "title" (escape title))
             <> el "body" (getStructureString content)
         )
     )
 
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape

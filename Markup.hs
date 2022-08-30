852a6c64f716955eba064dbef66e3c844580ffd8
Igor Uvarov <kuflash@ya.ru>
2022-08-30 11:14:56 +0300

    chapter 4.2

--- Markup.hs
+++ Markup.hs
@@ -16,6 +16,22 @@ data Structure
   | OrderedList [String]
   | CodeBlock [String]
 
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let paragraph = Paragraph (unlines (reverse currentParagraph))
+   in case txts of
+        [] -> [paragraph]
+        currentLine : rest ->
+          if trim currentLine == ""
+            then paragraph : parseLines [] rest
+            else parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
+
 example1 :: Document
 example1 = [Paragraph "Hello, world!"]
 

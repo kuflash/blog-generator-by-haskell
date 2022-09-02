f26a4f47de63db93ef6ebb57a37f9a396c8a60fb
Igor Uvarov <kuflash@ya.ru>
2022-09-02 19:06:39 +0300

    chapter 5.2

---
+++ Main.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import qualified Convert
+import qualified Html
+import qualified Markup
+import System.Directory (doesDirectoryExist, doesFileExist)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> do
+      content <- getContents
+      putStrLn $ process "Default title" content
+    [input, output] -> do
+      content <- getContents
+      isExist <- doesDirectoryExist output
+      let writeResult = writeFile output (process input content)
+      if isExist
+        then whenIO confirm writeResult
+        else writeResult
+    _ ->
+      putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+
+process :: Html.Title -> String -> String
+process title txt =
+  Html.render $ Convert.convert title $ Markup.parse txt
+
+confirm :: IO Bool
+confirm =
+  putStrLn "Are you sure? (y/n)"
+    *> getLine >>= \answer ->
+      case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _ ->
+          putStrLn "Invalid response. use y or n"
+            *> confirm
+
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action =
+  cond >>= \result ->
+    if result
+      then action
+      else pure ()
